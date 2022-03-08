650f52860bc64bc94d3712c6546537a786cc4887
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2022-03-08 10:28:50 -0500

    Update knn.py

--- solution/from_scratch/knn.py
+++ solution/from_scratch/knn.py
@@ -1,4 +1,5 @@
 import numpy as np
+from typing import Optional
 
 
 class KNN:
@@ -7,11 +8,11 @@ class KNN:
 		self.X = None
 		self.y = None
 
-	def fit(self, X, y):
+	def fit(self, X: np.ndarray, y: np.ndarray):
 		self.X = X
 		self.y = y
 
-	def predict(self, X):
+	def predict(self, X: np.ndarray, y: Optional[np.ndarray] = None) -> np.ndarray:
 		out = []
 		for x in X:
 			dist = np.sum((self.X - x)**2, axis=-1)
@@ -20,7 +21,11 @@ class KNN:
 			y_nn = self.y[k_indexes]
 			y_nn, y_nn_idx = np.unique(y_nn, return_counts=True)
 			out.append(y_nn[np.argmax(y_nn_idx)])
-		return np.asarray(out)
+		out = np.asarray(out)
+		if y is not None:
+			acc = np.isclose(np.abs(y - out), 0).astype(dtype=int).sum() / y.shape[0]
+			return out, acc
+		return out
 
 
 
